2b69f970f4246595a9f51e0f272c5d3213fffdde
ABINAYA1006 <abinaya106200@gmail.com>
2026-09-03 21:02:13 +0530

    Added user input program

---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Input{
+    public static void main(String[]args){
+        Scanner input =new Scanner(System.in);
+        System.out.print("please enter some input: ");
+        int rollno=input.nextInt();
+        System.out.println("your roll number is " +rollno);
+    }
+}
