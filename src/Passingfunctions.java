b2c9ed8f1364d94f6cbf8a5d1a11ebc1d60b35e6
ABINAYA1006 <Abinaya106200@gmail.com>
2026-09-19 06:25:40 +0530

    Added multidim program

--- src/Passingfunctions.java
+++ src/Passingfunctions.java
@@ -5,7 +5,7 @@ public static void main(String[]args){
     int[]nums={3,4,5,6};
     System.out.println(Arrays.toString(nums));
     change(nums);
-    System.out.println(Arrays.toString(nums));git commit -m "Added passing functions program"
+    System.out.println(Arrays.toString(nums));
 }
  static void change(int[]arr){
     arr[0]=99;
