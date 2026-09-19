583977a03d3b30a4c58c1b9a83e403b51191cfa1
ABINAYA1006 <Abinaya106200@gmail.com>
2026-09-19 05:36:16 +0530

    Added passing functions program

--- src/Passingfunctions.java
+++ src/Passingfunctions.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
+import java.util.Arrays;
 public class Passingfunctions {
+public static void main(String[]args){
+    int[]nums={3,4,5,6};
+    System.out.println(Arrays.toString(nums));
+    change(nums);
+    System.out.println(Arrays.toString(nums));git commit -m "Added passing functions program"
+}
+ static void change(int[]arr){
+    arr[0]=99;
+}
 }
