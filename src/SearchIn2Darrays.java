2da6bc1cd38cd07f910e8a38f5ee5c61274efbb6
ABINAYA1006 <Abinaya106200@gmail.com>
2026-09-23 20:08:54 +0530

    Added 2Darray program

---
+++ src/SearchIn2Darrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.Arrays;
+public class SearchIn2Darrays {
+    public static void main (String[]args){
+    int [][]arr={{23,4,1},{18,12,3,9},{78,99,34,56},{18,12,1}};
+    int  target=56;
+    int [] ans =Search(arr,target);
+    System.out.println(Arrays.toString(ans));
+}
+static int []Search(int[][]arr,int target){
+    for(int row=0;row<arr.length;row++){
+        for(int col=0;col<arr[row].length;col++){
+            if(arr[row][col]==target){
+                return new int []{row,col};
+            }
+        }
+    }
+    return new int[]{-1,-1};
+    }
+}
