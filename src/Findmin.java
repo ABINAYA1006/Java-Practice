db6468a9ded5ab9082b0e11f79bd3b00e23e8e28
ABINAYA1006 <Abinaya106200@gmail.com>
2026-09-23 16:12:46 +0530

    added program

--- src/Findmin.java
+++ src/Findmin.java
@@ -4,6 +4,7 @@ public class Findmin {
         int [ ]arr ={18,2,7,3,14,23};
         System.out.println(min(arr));
     }
+
     static int min(int[]arr){
         int ans=arr[0];
         for(int i=1;i<arr.length;i++){
